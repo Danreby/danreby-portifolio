63d6036da083983f488a5d9654efbf3a1a9cedbc
Bernardo Rolim <rolimbernardo6@gmail.com>
2025-05-12 16:07:44 -0300

    inicio projeto 1

--- routes/web.php
+++ routes/web.php
@@ -1,16 +1,7 @@
 <?php
 
-use App\Http\Controllers\DashboardController;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Aqui você registra as rotas que serão carregadas pelo middleware "web".
-|
-*/
-
-// Faz com que o '/' carregue diretamente a view 'dashboard'
-Route::get('/', [DashboardController::class, 'index'])->name('home');
+Route::get('/', function () {
+    return view('welcome');
+});
