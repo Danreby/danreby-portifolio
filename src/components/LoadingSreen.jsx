cf8dcf5578092141eaaf95e91d971c854a23d8d1
Bernardo Rolim <rolimbernardo6@gmail.com>
2025-07-17 16:33:30 -0300

    Alterando pagina principal

--- src/components/LoadingSreen.jsx
+++ src/components/LoadingSreen.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 
 export const LoadingScreen = ({onComplete}) => {
     const [text, setText] = useState("");
-    const fullText = "<Hello World />";
+    const fullText = "Yokoso, Watashi no ryōiki Tenkai e";
 
     useEffect(() => {
         let index = 0;
@@ -24,7 +24,7 @@ export const LoadingScreen = ({onComplete}) => {
     
   return (
     <div className="fixed inset-0 z-50 bg-black text-gray-100 flex flex-col items-center justify-center">
-      <div className="mb-4 text-4xl font-mono font-bold">{text}<span className="animate-blink ml-1">|</span></div>
+      <div className="mb-4 text-4xl font-mono font-bold bg-gradient-to-r from-blue-500 to-purple-600 bg-clip-text text-transparent leading-right uppercase">{text}<span className="animate-blink ml-1">|</span></div>
       <div className="w-[200px] h-[2px] bg-gray-800 rounded relative overflow-hidden">
         <div className="w-[40%] h-full bg-blue-500 shadow-[0_0_15px_#3b82f6] animate-loading-bar">
             {/* {""} */}
--- src/components/sections/Home.jsx
+++ src/components/sections/Home.jsx
@@ -6,7 +6,7 @@ export const Home = () => {
       <RevealOnScroll>
         <div className="text-center z-10 px-4">
           <h1 className="text-5xl font-bold mb-6 bg-gradient-to-r from-blue-500 to-purple-600 bg-clip-text text-transparent leading-right uppercase">
-              Yokoso, Watashi no ryōiki Tenkai e
+              Bernardo Santos Rolim
           </h1>
           <p className="text-gray-400 text-lg mb-8 max-w-lg mx-auto">
               Explore meus projetos e saiba mais sobre mim.
