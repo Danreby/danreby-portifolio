10a5be4351d8309d1c845d2813e0c093977a2912
Bernardo Rolim <rolimbernardo6@gmail.com>
2025-10-14 15:51:57 -0300

    Colocando "cobrinhas" na tela

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import "./App.css";
-import { LoadingScreen } from "./components/LoadingSreen";
 import "./index.css";
+import { LoadingScreen } from "./components/LoadingSreen";
 import { NavBar } from "./components/NavBar";
 import { MobileMenu } from "./components/MobileMenu";
 import { Home } from "./components/sections/Home";
@@ -9,16 +9,23 @@ import { About } from "./components/sections/About";
 import { Projects } from "./components/sections/Projects";
 import { Contact } from "./components/sections/Contact";
 import { FindMe } from "./components/sections/FindMe";
+import SnakeLinesBackground from "./components/common/SnakeLinesBackground";
 
 function App() {
-  const [isLoaded, setIsLoaded] = useState(false); 
+  const [isLoaded, setIsLoaded] = useState(false);
   const [menuOpen, setMenuOpen] = useState(false);
-  const [language, setLanguage] = useState('pt') 
+  const [language, setLanguage] = useState("pt");
 
   return (
     <>
-      {!isLoaded && <LoadingScreen onComplete={() => setIsLoaded(true)} />} {""}
-        <div className={`min-h-screen transition-opacity duration-700 ${isLoaded ? "opacity-100" : "opacity-0"} bg-black text-gray-100`}>
+      {!isLoaded && <LoadingScreen onComplete={() => setIsLoaded(true)} />}
+      <div
+        className={`min-h-screen transition-opacity duration-700 ${
+          isLoaded ? "opacity-100" : "opacity-0"
+        } bg-black text-gray-100 relative`}
+      >
+        <SnakeLinesBackground/>
+        <div className="relative z-10">
           <NavBar menuOpen={menuOpen} setMenuOpen={setMenuOpen} setLanguage={setLanguage} language={language}/>
           <MobileMenu menuOpen={menuOpen} setMenuOpen={setMenuOpen} setLanguage={setLanguage} language={language}/>
           <Home language={language}/>
@@ -27,8 +34,9 @@ function App() {
           <FindMe language={language}/>
           <Contact language={language}/>
         </div>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
